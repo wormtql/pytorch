dcd7b7ace394585ad1d367abb9bbdb85f93d8d82
Bo Wang <bowangbj@fb.com>
2021-10-11 22:03:49 -0700

    Add torch.nn.init.uniform_ operator to ShardedTensor.
    Summary:
    Use torch_function to extend torch.nn.init.uniform_
    The Init is done in SPMD fashion. Note that ideally we want to aggregate sharded tensors into a global tensor, init it and reshard. It's fine to run it SPMD since uniform is I.I.D indepenent and identifically distributed.
    Also enable unit test for test_linear.py for OSS test
    
    Test Plan:
    
    a) Unit Test
    (pytorch) ... $ python test/distributed/_sharded_tensor/ops/test_init.py TestShardedTensorNNInit  --v
    (pytorch) ... $ python test/distributed/_sharded_tensor/ops/test_linear.py --v  (before runs this command is no-op)
    
    or b) Manual run:  Instruction here: https://docs.google.com/document/d/1_m1Hdo5w51-hhPlZ_F8Y6PIWrN7UgJZqiSpARYvhsaE/edit#
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:
    
    ghstack-source-id: 87e997039919ea423e3b139d346b62136a3ac241
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/63997

--- torch/distributed/_sharded_tensor/api.py
+++ torch/distributed/_sharded_tensor/api.py
@@ -26,7 +26,7 @@ from torch.distributed._sharding_spec._internals import (
     get_chunked_dim_size,
 )
 from torch.types import Number
-from .ops import sharded_linear
+from .ops import (sharded_linear, uniform_)
 
 # Tracking for sharded tensor objects.
 _sharded_tensor_lock = threading.Lock()
@@ -635,7 +635,8 @@ class ShardedTensor(object):
     def __torch_function__(self, func, types, args=(), kwargs=None):
         if func == torch.nn.functional.linear:
             return sharded_linear(types, args, kwargs, self._process_group)
-
+        elif func == torch.nn.init.uniform_:
+            return uniform_(types, args, kwargs)
         raise RuntimeError(
             f"torch function '{func.__name__}', with args: {args} and "
             f"kwargs: {kwargs} not supported for ShardedTensor!")
